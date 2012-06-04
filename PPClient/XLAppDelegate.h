759d5961cfb1c9007d8b2e00a8b7eceec62c2043
Albert <xieliang43@126.com>
2012-06-04 13:30:08 +0800

    create project

---
+++ PPClient/XLAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  XLAppDelegate.h
+//  PPClient
+//
+//  Created by xie liang on 6/4/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PPClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PPClient
+//
+//  Created by xie liang on 6/4/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XLAppDelegate class]));
+    }
+}
